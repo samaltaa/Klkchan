e4fc1020f99368202328c18a14e1123474cf4f68
altagracia <samirasanchez8090@gmail.com>
2025-08-12 18:34:28 -0400

    local data querying from json defined and set up

--- app/app.py
+++ app/app.py
@@ -1,9 +1,44 @@
 from fastapi import FastAPI
-from fastapi import Query
-from .routers.users import users_router
-from .routers.posts import posts_router
-from .routers.test import test_router
-from .routers.myupload import upload_router
-from .routers import models_docs
+from schemas import *
+import json
+import pathlib as path
 
+"""
+I have created relative file paths because what works on my 
+machine might not work on yours, when creating docker images later
+this will be the best practice since it keeps the folder 
+structure consistent 
+"""
+
+BASE_DIR = path(__file__).resolve().parent.parent #efining parent directory 
+
+DATA_FILE = path(__file__).resolve().parent / "data" / "data.json" #pointing to the data file and directory 
+DATA_FILE.parent.mkdir(exist_ok=True)
+
+app = FastAPI()
+
+#function for getting the data from the json file 
+def load_data():
+    if DATA_FILE.exists():
+        with open(DATA_FILE, 'r') as f:
+            return json.load(f)
+        return {"boards":[], "users": [], "posts": [], "comments": [], "replies": []}
+
+#function for saving data in the json file
+def save_data(data):
+    with open(DATA_FILE, 'W') as f:
+        json.dump(data, f, indent=2)
+
+@app.on_event("startup")
+async def startup():
+    # Initialize JSON file if it doesn't exist
+    if not DATA_FILE.exists():
+        initial_data = {"boards":[], "users": [], "posts": [], "comments": [], "replies": []}
+        save_data(initial_data)
+
+@app.get("/health")
+async def health():
+    return {"status": "ok"}
+
+#endpoints
 
