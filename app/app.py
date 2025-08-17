675d46e05dfa6ece73eb2f8cf2bc39dac6c5111c
altagracia <samirasanchez8090@gmail.com>
2025-08-17 12:20:39 -0400

    board endpoints

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from schemas import *
 import json
-import pathlib as path
+from pathlib import Path
 
 """
 I have created relative file paths because what works on my 
@@ -10,9 +10,9 @@ this will be the best practice since it keeps the folder
 structure consistent 
 """
 
-BASE_DIR = path(__file__).resolve().parent.parent #efining parent directory 
+BASE_DIR = Path(__file__).resolve().parent.parent #efining parent directory 
 
-DATA_FILE = path(__file__).resolve().parent / "data" / "data.json" #pointing to the data file and directory 
+DATA_FILE = Path(__file__).resolve().parent / "data" / "data.json" #pointing to the data file and directory 
 DATA_FILE.parent.mkdir(exist_ok=True)
 
 app = FastAPI()
@@ -26,7 +26,7 @@ def load_data():
 
 #function for saving data in the json file
 def save_data(data):
-    with open(DATA_FILE, 'W') as f:
+    with open(DATA_FILE, 'w') as f:
         json.dump(data, f, indent=2)
 
 @app.on_event("startup")
@@ -40,5 +40,29 @@ async def startup():
 async def health():
     return {"status": "ok"}
 
-#endpoints
+#endpoints related to boards
+@app.get("/")
+async def get_data():
+    data = load_data()
+
+    return data
+
+@app.post("/postboard")
+async def post_board(payload: BoardCreate):
+    data = load_data()
+
+    new_board = {
+        "name": payload.name,
+        "description": payload.name,
+    }
+    data["boards"].append(new_board)
+
+    save_data(data)
+    return data
+
+@app.get("/getboards")
+async def get_boards():
+    data = load_data()
+    return {"boards": data["boards"]}
+
 
